d77376188fdfacac0bad0e80a9da75fae296be87
Aleksandr Petrov <ieropael@gmail.com>
2021-04-13 01:18:52 +0400

    feat: add "04-mine-sweeper" task solution

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -4,11 +4,11 @@
  * in the neighboring cells. Starting off with some arrangement of mines
  * we want to create a Minesweeper game setup.
  *
- * @param {Array<Array>} matrix
+ * @param {Array<Array>} field
  * @return {Array<Array>}
  *
  * @example
- * matrix = [
+ * field = [
  *  [true, false, false],
  *  [false, true, false],
  *  [false, false, false]
@@ -21,8 +21,31 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const field = matrix.slice(0);
+  for (let i = 0; i < field.length; i++) {
+    for (let j = 0; j < field[i].length; j++) {
+      if (!field[i][j]) {
+        for (let k = i - 1; k <= i + 1; k++) {
+          for (let l = j - 1; l <= j + 1; l++) {
+            if (k >= 0 && l >= 0 && k < field.length && l < field[i].length) {
+              if (field[k][l] === true) {
+                field[i][j]++;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  for (let i = 0; i < field.length; i++) {
+    for (let j = 0; j < field[i].length; j++) {
+      if (field[i][j] === true) {
+        field[i][j] = 1;
+      }
+    }
+  }
+  return field;
 }
 
 module.exports = minesweeper;
